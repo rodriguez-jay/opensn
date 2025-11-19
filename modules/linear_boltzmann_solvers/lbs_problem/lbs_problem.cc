cd8565e643ae68347966635abef54d82919b6158
rodriguez-jay <j2jason23@gmail.com>
2025-11-19 10:30:32 -0600

    Adj Fix

--- modules/linear_boltzmann_solvers/lbs_problem/lbs_problem.cc
+++ modules/linear_boltzmann_solvers/lbs_problem/lbs_problem.cc
@@ -698,10 +698,6 @@ LBSProblem::SetOptions(const InputParameters& input)
 
     else if (spec.GetName() == "field_function_prefix")
       options_.field_function_prefix = spec.GetValue<std::string>();
-
-    else if (spec.GetName() == "adjoint")
-      options_.adjoint = spec.GetValue<bool>();
-
   } // for p
 
   if (options_.restart_writes_enabled)
